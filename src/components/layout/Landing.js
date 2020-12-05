7f623be75477ce5888a3a2b60bd5ebca2590f0d3
Toby Boudreaux <tobynboudreaux@gmail.com>
2020-12-05 17:08:07 -0600

    Trying Content-Security-Policy

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -8,7 +8,12 @@ export default class Landing extends React.Component {
       }
 
       componentDidMount() {
-        fetch('//3.139.235.28:8080/project0/product')
+        fetch('http://3.139.235.28:8080/project0/product', {
+            method: "GET",
+            headers: {
+                'Content-Security-Policy': 'upgrade-insecure-requests'
+            }
+        })
         .then(res => res.json())
         .then((out) => {
           console.log( out);
